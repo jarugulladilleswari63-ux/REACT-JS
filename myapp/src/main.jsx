6c98cf199904d3fc79c9a1700afde103b1c34b48
Dilleswari <jarugulladilleswari63@gmail.com>
2026-08-11 11:19:49 +0530

    updated react

--- myapp/src/main.jsx
+++ myapp/src/main.jsx
@@ -1,10 +1,9 @@
-// import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-// import './index.css'
-import App from './App.jsx'
+// !==first way to create a createroot()
+// import ReactDOM from "react-dom/client"
+// let r = ReactDOM.createRoot(document.getElementById("root"))
+// console.log(r);
+// r.render(<h1>hello react js</h1>)
 
-createRoot(document.getElementById('root')).render(
-  
-    <App />
-
-)
+// !=== secondway of createroot()
+import {createRoot} from "react-dom/client"
+createRoot(document.getElementById("root")).render(<h2>Hello react javascript</h2>)
