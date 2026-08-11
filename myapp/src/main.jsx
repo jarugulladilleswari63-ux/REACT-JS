bce0210bb756971530713531df2bfec3c750d59a
Dilleswari <jarugulladilleswari63@gmail.com>
2026-08-11 17:58:22 +0530

    updated react

--- myapp/src/main.jsx
+++ myapp/src/main.jsx
@@ -5,5 +5,17 @@
 // r.render(<h1>hello react js</h1>)
 
 // !=== secondway of createroot()
-import {createRoot} from "react-dom/client"
-createRoot(document.getElementById("root")).render(<h2>Hello react javascript</h2>)
+// import {createRoot} from "react-dom/client"
+// createRoot(document.getElementById("root")).render(<h2>Hello react</h2>)
+
+import { createRoot } from "react-dom/client";
+import React from "react";
+
+// element creation in react js 
+// 1. react.createElement(element,properties /attritube,children)
+
+let element = React.createElement("h1",null,"hii react js")
+let ele2 = React.createElement("h2",null,"react is library of js")
+
+let container = React.createElement("div",null,element,ele2)
+createRoot(document.getElementById("root")).render(container)
