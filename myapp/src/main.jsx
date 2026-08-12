e3828e5bddd7962b5c5f71081d30e73c9c1f58e3
Dilleswari <jarugulladilleswari63@gmail.com>
2026-08-12 10:30:26 +0530

    Updated REACT-JS

--- myapp/src/main.jsx
+++ myapp/src/main.jsx
@@ -8,14 +8,24 @@
 // import {createRoot} from "react-dom/client"
 // createRoot(document.getElementById("root")).render(<h2>Hello react</h2>)
 
-import { createRoot } from "react-dom/client";
-import React from "react";
+// import { createRoot } from "react-dom/client";
+// import React from "react";
 
 // element creation in react js 
 // 1. react.createElement(element,properties /attritube,children)
 
-let element = React.createElement("h1",null,"hii react js")
-let ele2 = React.createElement("h2",null,"react is library of js")
+// let element = React.createElement("h1",null,"hii react js")
+// let ele2 = React.createElement("h2",null,"react is library of js")
 
-let container = React.createElement("div",null,element,ele2)
-createRoot(document.getElementById("root")).render(container)
+// let container = React.createElement("div",null,element,ele2)
+// createRoot(document.getElementById("root")).render(container)
+
+
+// ! --- creating multiple elements inside one createroot 
+import {createRoot} from "react-dom/client";
+import { createElement } from "react";
+createRoot(document.getElementById("root")).render(createElement("div",{style:{border:"2px solid black",width:"300px",display:"flex",flexDirection:"column"},alignItems:"center"},
+    createElement("img",{src:"https://media-public.canva.com/MADGyEZRjKc/4/thumbnail_large.jpg"}),
+    createElement("h1",null,"sky is beautiful"),
+    createElement("h2",null,"sky❤️")
+))
