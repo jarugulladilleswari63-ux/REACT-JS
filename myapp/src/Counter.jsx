c0158b38e3dc7dd0f69f77df9b3887865b22bc5b
Dilleswari <jarugulladilleswari63@gmail.com>
2026-08-14 11:25:04 +0530

    update REACT-JS

--- myapp/src/Counter.jsx
+++ myapp/src/Counter.jsx
@@ -1,6 +1,13 @@
-
+import { useState } from "react";
 let Counter = ()=>{
-    return <>Counter Component </>
+    let [value,Updater]= useState(0);
+    console.log("component");
+
+    return <>
+    <h1>Counter Component </h1>
+    <h2>count  {value} </h2>
+    <button onClick={()=>Updater(value+1)}>Increment</button>
+    </>
 }
 
 export default Counter;
